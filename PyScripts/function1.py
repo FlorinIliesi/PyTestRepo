ab209dd86de6d04b093197850f525f2abcc8f5c3
FlorinIliesi <50583155+FlorinIliesi@users.noreply.github.com>
2019-06-19 19:51:11 +0300

    file update and requirements

---
+++ PyScripts/function1.py
@@ -0,0 +1,6 @@
+# create functions in python that will take an argument and will return 1 for True or 0 for False for the following checks:
+# - if the entry is numberic
+# - if the entry is alphanumberic (chars and numbers)
+# - if the entry is chars only
+# - if entry is lowwercase
+# - if entry is uppercase
